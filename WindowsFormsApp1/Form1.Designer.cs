a1a8e3e0c810413311320b31c5ee440642db76d8
FurkanYazbahar <32523353+FurkanYazbahar@users.noreply.github.com>
2018-12-24 21:14:54 +0300

    parse/rander done

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -38,20 +38,15 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.linkLabel3 = new System.Windows.Forms.LinkLabel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(37, 346);
+            this.button1.Location = new System.Drawing.Point(239, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 47);
+            this.button1.Size = new System.Drawing.Size(88, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "AÇ";
             this.button1.UseVisualStyleBackColor = true;
@@ -59,9 +54,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(619, 346);
+            this.button2.Location = new System.Drawing.Point(239, 67);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(97, 47);
+            this.button2.Size = new System.Drawing.Size(88, 31);
             this.button2.TabIndex = 1;
             this.button2.Text = "KAYDET";
             this.button2.UseVisualStyleBackColor = true;
@@ -70,9 +66,10 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(587, 25);
+            this.linkLabel1.Location = new System.Drawing.Point(86, 21);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(58, 20);
+            this.linkLabel1.Size = new System.Drawing.Size(39, 13);
             this.linkLabel1.TabIndex = 2;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Ayarlar";
@@ -80,21 +77,22 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(516, 62);
+            this.radioButton1.Location = new System.Drawing.Point(39, 45);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(69, 24);
+            this.radioButton1.Size = new System.Drawing.Size(49, 17);
             this.radioButton1.TabIndex = 3;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "4:4:4";
             this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(591, 62);
+            this.radioButton2.Location = new System.Drawing.Point(89, 45);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(69, 24);
+            this.radioButton2.Size = new System.Drawing.Size(49, 17);
             this.radioButton2.TabIndex = 4;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "4:2:2";
@@ -103,9 +101,10 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(685, 62);
+            this.radioButton3.Location = new System.Drawing.Point(152, 45);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(69, 24);
+            this.radioButton3.Size = new System.Drawing.Size(49, 17);
             this.radioButton3.TabIndex = 5;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "4:2:0";
@@ -113,24 +112,27 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(607, 113);
+            this.textBox1.Location = new System.Drawing.Point(100, 78);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(137, 26);
+            this.textBox1.Size = new System.Drawing.Size(93, 20);
             this.textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(607, 158);
+            this.textBox2.Location = new System.Drawing.Point(100, 108);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(137, 26);
+            this.textBox2.Size = new System.Drawing.Size(93, 20);
             this.textBox2.TabIndex = 7;
             // 
             // linkLabel2
             // 
             this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(489, 119);
+            this.linkLabel2.Location = new System.Drawing.Point(21, 82);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(29, 20);
+            this.linkLabel2.Size = new System.Drawing.Size(20, 13);
             this.linkLabel2.TabIndex = 8;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "En";
@@ -138,65 +140,30 @@
             // linkLabel3
             // 
             this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(489, 164);
+            this.linkLabel3.Location = new System.Drawing.Point(21, 112);
+            this.linkLabel3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(36, 20);
+            this.linkLabel3.Size = new System.Drawing.Size(25, 13);
             this.linkLabel3.TabIndex = 9;
             this.linkLabel3.TabStop = true;
             this.linkLabel3.Text = "Boy";
-            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
             // 
-            // pictureBox1
+            // button3
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(37, 62);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(302, 222);
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(550, 199);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(238, 69);
-            this.trackBar1.TabIndex = 11;
-            // 
-            // linkLabel4
-            // 
-            this.linkLabel4.AutoSize = true;
-            this.linkLabel4.Location = new System.Drawing.Point(489, 209);
-            this.linkLabel4.Name = "linkLabel4";
-            this.linkLabel4.Size = new System.Drawing.Size(20, 20);
-            this.linkLabel4.TabIndex = 12;
-            this.linkLabel4.TabStop = true;
-            this.linkLabel4.Text = "Y";
-            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(544, 257);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 26);
-            this.textBox3.TabIndex = 13;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(409, 246);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
-            this.richTextBox1.TabIndex = 14;
-            this.richTextBox1.Text = "";
+            this.button3.Location = new System.Drawing.Point(239, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 31);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "OYNAT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.linkLabel4);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(361, 192);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.linkLabel3);
             this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.textBox2);
@@ -207,10 +174,10 @@
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,11 +195,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.LinkLabel linkLabel2;
         private System.Windows.Forms.LinkLabel linkLabel3;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.LinkLabel linkLabel4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,231 +8,185 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-
-
+using System.Drawing.Imaging;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        int height, width;
+        FileStream fs;
+        Byte[] fileArray , yArray;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
-
+            //kullanıcı aç butonuna bastığı zaman 
+             dosyaAc();
         }
-
-        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-
-        }
-
-       
-      
-
-        static long GetFileSize(string FilePath)
-        {
-            if (File.Exists(FilePath))
-            {
-                return new FileInfo(FilePath).Length;
-            }
-            return 0;
-        }
-
-
-
-        private void button1_Click(object sender, EventArgs e)//kullanıcı aç butonuna bastığı zaman öncelikle eğer 4:2:0 seçili ise 
-                                                              //.yuv uzantılı dosyamızı açıyoruz en boyy değerlerini textbox içerisinden alıyoruz ve frame denilen yerdeçerçeve sayısını tutuyor
-        {
-            if (radioButton3.Checked == true)
-            {
-                OpenFileDialog myOpenFileDlg = new OpenFileDialog();
-                myOpenFileDlg.Filter = "YUV files (*.yuv)|*.yuv";
-                pictureBox1.Width = int.Parse(textBox1.Text);
-                pictureBox1.Height = int.Parse(textBox2.Text);
-                int nSize = pictureBox1.Width * pictureBox1.Height;
-                int frameSize = Convert.ToInt32((pictureBox1.Width * pictureBox1.Height) * 1.5);
-                int input;
-                int[,] y = new int[pictureBox1.Width, pictureBox1.Height];
-                int[,] u = new int[pictureBox1.Width/2, pictureBox1.Height/2];
-                int[,] v = new int[pictureBox1.Width/2, pictureBox1.Height/2];
-
-                if (myOpenFileDlg.ShowDialog() == DialogResult.OK)
-                {
-
-
-                    FileStream infile = File.Open(myOpenFileDlg.FileName, FileMode.Open, FileAccess.Read, FileShare.Read);
-                    pictureBox1.ImageLocation = myOpenFileDlg.FileName;
-
-
-                    while (infile.Position != infile.Length)
-                    {
-                        input = infile.ReadByte();
-                        for (int i = 0; i < pictureBox1.Height; i++)
-                            for (int j = 0; j < pictureBox1.Width; j++)
-                                y[j, i] = input;
-                        for (int i = 0; i <pictureBox1.Height / 2; i++)
-                            for (int j = 0; j <  pictureBox1.Width / 2; j++)
-                                u[j, i] = input;
-                        for (int i = 0; i < pictureBox1.Height/ 2; i++)
-                            for (int j = 0; j < pictureBox1.Width  / 2; j++)
-                                v[j, i] = input;
-                        frameSize++;
-                    }
-
-                    for (int i = 0; i <  pictureBox1.Height; i++)
-                        for (int j = 0; j <pictureBox1.Width; j++)
-                            richTextBox1.AppendText(y[j, i].ToString() + "," + u[j / 2, i / 2].ToString() + "," + v[j / 2, i / 2].ToString());
-                    textBox3.Text = frameSize.ToString();
-                }
-
-                /*0
-
-
-
-
-
-                FileStream fs = new FileStream(dosyaAdi, FileMode.Open, FileAccess.Read);
-
-
-
-
-
-                int y = pictureBox1.Width * pictureBox1.Height;
-                int u = pictureBox1.Width * pictureBox1.Height/4;
-                int v= pictureBox1.Width * pictureBox1.Height/4;
-              //yuv dan rgb ye dönüşüm
-              int R = Convert.ToInt32((y + 1.139837398373983740 * v) * 255);
-             int G = Convert.ToInt32((y - 0.3946517043589703515 * u - 0.5805986066674976801 * v) * 255);
-              int B = Convert.ToInt32((y + 2.032110091743119266 * u) * 255);
-              int frame = (int)GetFileSize(sec.ToString()) / frameSize;
-
-                Image img = pictureBox1.Image;
-                Bitmap bmp = new Bitmap(img);
-                int[] kirmizi = new int[Convert.ToInt32((y + 1.139837398373983740 * v) * 255)];
-                int[] yesil = new int[Convert.ToInt32((y - 0.3946517043589703515 * u - 0.5805986066674976801 * v) * 255)];
-                int[] mavi = new int[Convert.ToInt32((y + 2.032110091743119266 * u) * 255)];
-
-                for (int i = 0; i < bmp.Size.Height; i++)
-                    for (int j = 0; j < bmp.Size.Width; j++)
-                    {
-                        Color renk = bmp.GetPixel(i, j);
-
-                        kirmizi[renk.R]++;
-
-                        yesil[renk.G]++;
-                        mavi[renk.B]++;
-                        byte[] yuv = new byte[(int)frameSize];
-                    }
-                //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
-                //2.parametre dosyanın açılacağını,
-                //3.parametre dosyaya erişimin veri okumak için olacağını gösterir.
-               StreamReader sw = new StreamReader(fs);
-                //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
-
-                pictureBox1.ImageLocation = sec.FileName;
-                //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
-                //Son satır okunduktan sonra okuma işlemini bitirdik
-
-                fs.Close();
-                //İşimiz bitince kullandığımız nesneleri iade ettik.
-            }
-
-        }
-        public static Bitmap GetPictureFromData(int w, int h, byte[] data, byte[][] palette)
-        {
-            Bitmap pic = new Bitmap(w, h, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            Color c;
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                byte[] color_bytes = palette[data[i]];
-                c = Color.FromArgb(color_bytes[0], color_bytes[1], color_bytes[2]);
-                pic.SetPixel(i,i, c);
-            }
-
-            return pic;
-        }
-
-
-
-
-    */
-            }
-            if (radioButton1.Checked == true)
-            {
-                OpenFileDialog myOpenFileDlg = new OpenFileDialog();
-                myOpenFileDlg.Filter = "YUV files (*.yuv)|*.yuv";
-                pictureBox1.Height = int.Parse(textBox1.Text);
-                pictureBox1.Width = int.Parse(textBox2.Text);
-                int nSize = pictureBox1.Width * pictureBox1.Height;
-                int frameSize = pictureBox1.Width * pictureBox1.Height;
-                int input;
-                int[,] y = new int[pictureBox1.Width,pictureBox1.Height];
-                int[,] u = new int[pictureBox1.Width, pictureBox1.Height];
-                int[,] v = new int[pictureBox1.Width, pictureBox1.Height];
-             
-
-                if (myOpenFileDlg.ShowDialog() == DialogResult.OK)
-                {
-
-
-                    FileStream infile = File.Open(myOpenFileDlg.FileName, FileMode.Open, FileAccess.Read, FileShare.Read);
-                    pictureBox1.ImageLocation = myOpenFileDlg.FileName;
-
-
-                    while (infile.Position != infile.Length)
-                    {
-                        input = infile.ReadByte();
-                        for (int i = 0; i < pictureBox1.Width; i++)
-                            for (int j = 0; j < pictureBox1.Height; j++)
-                                y[j, i] = input;
-                        for (int i = 0; i < pictureBox1.Width; i++)
-                            for (int j = 0; j < pictureBox1.Height; j++)
-                                u[j, i] = input;
-                        for (int i = 0; i < pictureBox1.Width; i++)
-                            for (int j = 0; j < pictureBox1.Height; j++)
-                                v[j, i] = input;
-                        frameSize++;
-                    }
-
-                    for (int i = 0; i < pictureBox1.Width; i++)
-                        for (int j = 0; j < pictureBox1.Height; j++)
-                            richTextBox1.AppendText(y[j, i].ToString() + "," + u[j, i ].ToString() + "," + v[j , i].ToString());
-                    textBox3.Text = frameSize.ToString();
-                }
-
-            }
-        }
-                    private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
-
-        }
-
+        
         private void button2_Click(object sender, EventArgs e)//kaydt butonuna basıldığı zaman
         {
+            Object[] parseFile;
 
-
-            SaveFileDialog sfd = new SaveFileDialog();//yeni bir kaydetme diyaloğu oluşturuyoruz.
-
-            sfd.Filter = "Bitmap(*.bmp)|*.bmp";//.bmp  olarak kayıt imkanı sağlıyoruz.
-
-            sfd.Title = "Kayıt";//diğaloğumuzun başlığını belirliyoruz.
-
-            sfd.FileName = "yuvfotograf";//kaydedilen resmimizin adını 'resim' olarak belirliyoruz.
-
-            DialogResult sonuç = sfd.ShowDialog();
-
-            if (sonuç == DialogResult.OK)
+            if (radioButton1.Checked == true)
             {
-                pictureBox1.Image.Save(sfd.FileName);//Böylelikle resmi istediğimiz yere kaydediyoruz.
-
+                try
+                {
+                    width = int.Parse(textBox1.Text);
+                    height = int.Parse(textBox2.Text);
+                    parseFile = arrayParse(fileArray, 3);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("En ve Boy bilgilerinin girilmesi gerek !!");
+                }
             }
+            if (radioButton2.Checked == true)
+            {
+                try
+                {
+                    width = int.Parse(textBox1.Text);
+                    height = int.Parse(textBox2.Text);
+                    parseFile = arrayParse(fileArray, 2);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("En ve Boy bilgilerinin girilmesi gerek !!");
+                }
+            }
+            if (radioButton3.Checked == true)
+            {
+                try
+                {
+                    width = int.Parse(textBox1.Text);
+                    height = int.Parse(textBox2.Text);
+                    parseFile = arrayParse(fileArray, 1.5);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("En ve Boy bilgilerinin girilmesi gerek !!");
+                }
+                
+            }
+            //if (bmpSave())
+            //{
+            //    MessageBox.Show("Kayıt işlemi başarılı !");
+            //}
+            //else
+            //    MessageBox.Show("Kayıt işlemi başarısız !");
         }
-       
-    } }
+
+        private Boolean bmpSave(Bitmap[] bmp)
+        {
+            return false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Show sh = new Show(height,width);
+            sh.Show();
+        }
+
+        // YUV formatındaki dosyaları seçer ve fileArray dizisine atar
+        public void dosyaAc ()
+        {
+            FileStream fileS;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "YUV files (*.yuv)|*.yuv";
+            file.InitialDirectory = "C:\\Users\\Furkan\\Desktop\\GitHub repo\\yuv Formatları";
+            int i = 0;
+            int sayac = 0;
+            try
+            {
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    fileS = File.Open(file.FileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    fileArray = new Byte[fileS.Length];
+                    while (i > -1)
+                    {
+                        i = fileS.ReadByte();
+                        if (i != -1)
+                        {
+                            fileArray[sayac] = (Byte)i;
+
+                            //MessageBox.Show($"{sayac}. indis :{ToHex(arr[sayac])}");
+                        }
+                        sayac++;
+                    }
+                }
+                if(fileArray != null)
+                {
+                MessageBox.Show("Dosya açma işlemi başarılı !!");
+                }
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Dosya açma işlemi başarısız !!");
+            }
+    
+        }
+
+        // Elimizdeki Byte dizisini istenilen yuv formatında bölümleme ve ayrıştırma yapıp nesne dizimize atıyoruz.
+        public Object[] arrayParse(Byte[] array , double yuvSize)
+        {
+            //TODO: yuv formatına göre bölümlencek 
+            ulong frameSize;
+            int ySize = height * width;
+            double frameS = height * width * yuvSize;
+            frameSize = Convert.ToUInt64(frameS);
+            MessageBox.Show($" frameCount :{array.Length/frameS}");
+            Object[] obj = null;
+            MessageBox.Show($"ySize : {ySize} frameSize :{frameSize}");
+
+            //Bitmap bmp;
+            //for (int frameCount = 0; frameCount < (arr.Length / frameSize); frameCount++)
+            //{
+            //    int tmp = 0;
+            //    for (int j = frameCount * frameSize; j < arr.Length; j++)
+            //    {
+            //        if (tmp < ySize)
+            //        {
+            //            arry[tmp] = arr[j];
+            //            tmp++;
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+            //    bmp = Convert2Bitmap(arry, 720, 576);
+            //    bmp.Save($"C:\\Users\\Furkan\\Desktop\\GitHub repo\\yuv Formatları\\4 4 4\\{frameCount}.bmp");
+            //}
+
+            return obj;
+        }
+
+        // Byte dizisini bitmap e dönüştürme
+        public Bitmap Convert2Bitmap(byte[] DATA, int width, int height)
+        {
+            Bitmap Bm = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            var b = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+            ColorPalette ncp = b.Palette;
+            for (int i = 0; i < 256; i++)
+                ncp.Entries[i] = Color.FromArgb(255, i, i, i);
+            b.Palette = ncp;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int Value = DATA[x + (y * width)];
+                    Color C = ncp.Entries[Value];
+                    Bm.SetPixel(x, y, C);
+                }
+            }
+            return Bm;
+        }
+    }
+}
 
